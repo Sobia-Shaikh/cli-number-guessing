d0519b9493bc29c30f492bc56b32c590edec4e71
Sobia-Shaikh <164778749+Sobia-Shaikh@users.noreply.github.com>
2024-03-26 16:47:19 +0500

    @cli-number

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+//1. coputer will generate number
+//2.User input for guessing number
+//3.computer user input with computer generated number and show result
+//const randomNumber=13;
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+//console.log(randomNumber);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "plz guess a number b/w 1-6",
+    },
+]);
+//consol.log(randomNumber);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
